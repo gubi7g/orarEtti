e282469d5a6fdf23ff7adc0f41e41e71de4a349e
Gabi Galer <gabigaler50@gmail.com>
2019-11-21 21:00:53 +0200

    curatenie + comentarii

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # orarEtti
 din csv -> JSON pt fiecare grupa
+
+exemplu output dorit:
+
+[   
+    {
+        'grupa': 'x',
+        'orar': {
+            'luni': {
+                '9-11': {
+                    'type': 'sem/curs',
+                    'course': 'AM2',
+                    'room': 'A03',
+                    ...
+                },
+                ...
+            },
+            ...
+        }
+    }
+]
--- test_orar.py
+++ test_orar.py
@@ -2,41 +2,7 @@ import json
 import csv
 import re
 
-# eu in range_orar am +1 celula in plus dupa grupele fiecarei serii.
-# in grupe eu nu am asa ceva, deci trebuie tinut cont de asta.
-# deci daca gasesc un curs/sem la indexul i, i se va atribui grupei i+
-
-# exemplu output dorit:
-
-# [{
-#     'grupa': 'x',
-#     'orar': {
-#         'luni': {
-#             '9-11': {
-#                 'type': 'sem/curs',
-#                 'course': 'AM2',
-#                 'room': 'A03'
-#             }
-#             ...
-#         }
-#         ...
-#     }
-#     'grupa': 'y',
-#     ...
-# ]
-
-
-def findGroupIndex(event_index, length_serii):
-    i = 0
-    s = 0
-    while(s + length_serii[i] < event_index):
-        s = s + length_serii[i] + 1
-        i = i + 1
-    return event_index - i - 1
-
-
 orar = []
-
 with open('orar_an1.csv') as f:
     reader = csv.reader(f)
 
@@ -46,6 +12,7 @@ with open('orar_an1.csv') as f:
 # taie chestiile useless
 orar = orar[:70]
 
+# serii & re patterns
 serii = list(filter(None, orar[0]))
 pattern_serie = re.compile(r'Anul \w{1,3} Seria \w{1}')
 
@@ -53,15 +20,13 @@ pattern_serie = re.compile(r'Anul \w{1,3} Seria \w{1}')
 serii = [x for x in serii if pattern_serie.findall(x)]
 nr_serii = len(serii)
 
+# grupe & re patterns
 grupe = list(filter(None, orar[1]))
-# print(grupe)
 pattern_grupa = re.compile(r'4\d{2}\w{1,2}')
-
 grupe = [x for x in grupe if pattern_grupa.findall(x)]
 nr_grupe = len(grupe)
 
-# trebuie sa stim cate grupe are fiecare serie (ajuta cu determinarea curs/sem):
-
+# trebuie sa stim cate grupe are fiecare serie
 length_serii = []
 dict_serii = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
 
@@ -72,9 +37,8 @@ for letter in dict_serii:
             tmp_sum = tmp_sum + 1
     length_serii.append(tmp_sum)
 
-
+# filter out trash
 effective_orar = nr_grupe + nr_serii
-# print(effective_orar)
 
 range_orar = [x[0:effective_orar+2]
               for x in orar[2:]]  # inclusiv effective_orar
@@ -85,65 +49,65 @@ for ind, i in enumerate(length_serii):
     temps = temps + i
     jump_cells.append(temps)
 
-# celule jump: 6, 19, 24
+# celule jump: 6, 19, 24...
 for ind, i in enumerate(jump_cells):
     jump_cells[ind] = jump_cells[ind] + ind + 2
 
+# adica indexul orei si al zilei
 jump_cells.append(0)
 jump_cells.append(1)
 
-# print(jump_cells)
-
-# print(zile)
-
-# print(zi_tmp)
 orar_final = [{} for x in grupe]
-
 ore_posibile_dict = []
 
+# won't use it, but a cool way to use f strings for zero-padding
 for i in range(8, 21):
     ore_posibile_dict.append(f'{i:02}-{(i+1):02}')
 
-
+# initialize a part of the final dict
 for grupa, orar_grupa in zip(grupe, orar_final):
     orar_grupa['grupa'] = grupa
     orar_grupa['orar'] = {}
     for zi in ['luni', 'marti', 'miercuri', 'joi', 'vineri']:
         orar_grupa['orar'][zi] = {}
-    # for interval in ore_posibile_dict:
-    #     orar_grupa['orar'][zi][interval] = {}
 
-# print(orar_final)
-# print(range_orar[1])
 zi_curenta = 'luni'
-jump_cells.sort(reverse=True)
-print(jump_cells)
-ora_curenta = 0
 
+# sortam invers pt a nu ne complica cu indexing gresit cand scoatem elemente din array
+jump_cells.sort(reverse=True)
+
+ora_curenta = 0
 
 # in range_orar[i][1] avem ora! oricare i
 for ind_int, interval in enumerate(range_orar):
+    # pe poz 0 avem numele zilei
     if interval[0] != '':
         zi_curenta = interval[0].lower()
+
+    # pe poz 1 avem intervalul orar
     if interval[1] != '':
         ora_curenta = interval[1]
+
+    # scoatem din campurile cu evenimente coloanele care nu ne folosesc (pt un zip bijectiv)
     for i in jump_cells:
         print(i)
         interval.pop(i)
+
     for (ind, cell), orar_grupa in zip(enumerate(interval), orar_final):
         if cell == '':
-            if cell != '':
-                # print(f'___celula ignorata___ {cell} la index {ind}:')
-                continue
+            continue
         else:
-            ind_grupa = findGroupIndex(ind, length_serii)
             # print(zi_curenta)
             if ora_curenta in orar_grupa['orar'][zi_curenta]:
                 orar_grupa['orar'][zi_curenta][ora_curenta]['course'] = cell
             else:
+                # daca nu este creat dict, avem eroare
+                # poate ramane doar else-ul asta in loc de if/else (cred)
                 orar_grupa['orar'][zi_curenta][ora_curenta] = {}
                 orar_grupa['orar'][zi_curenta][ora_curenta]['course'] = cell
 
             # print(f'grupa {grupe[ind_grupa]} are {cell}')
+
+# write everything to a file
 with open('orar_an1.json', 'w') as f:
     f.write(json.dumps(orar_final))
