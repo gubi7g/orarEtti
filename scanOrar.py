1c12fb9138254877dd29bda3c86f945fa5058ed7
Gabi Galer <gabigaler50@gmail.com>
2020-01-20 14:18:25 +0200

    resized orar cells with simple fun

--- scanOrar.py
+++ scanOrar.py
@@ -4,6 +4,16 @@ import cv2
 import numpy as np
 import os
 
+def getCoordinatedFromCropped(original, cropped, pointsx, pointsy):
+    kx = round(original.shape[:2][1] / cropped.shape[:2][1])
+    ky = round(original.shape[:2][0] / cropped.shape[:2][0])
+
+    new_pointsx = [kx * p for p in pointsx]
+    new_pointsy = [ky * p for p in pointsy]
+
+    return (min(new_pointsx), min(new_pointsy)), (max(new_pointsx), max(new_pointsy)) # (x, y), (x+w, y+h)
+
+
 def findBottomLeftPointOrar(cnts, img):
     tmpX = img.shape[:2][1] # width
     tmpY = 0
@@ -58,6 +68,8 @@ destSave = 'res/orarJpg.jpg'
 boundaries = [([204, 204, 204], [204, 204, 204])]
 
 img = cv2.imread(destSave)
+resized = img.copy()
+final_orar = img.copy()
 resized = imutils.resize(img, width=1500)
 
 (lower, upper) = boundaries[0]
@@ -95,6 +107,8 @@ for cnt in contours:
     if not cv2.contourArea(cnt) > 20:
         continue
 
+    # not necessary:
+
     x, y, *_ = cv2.boundingRect(cnt)
     cv2.circle(blank_image, (x, y), 1, (0, 255, 0), 1) # mai bun decat draw contours
     # momentan nu mai am nevoie de textul scris in coloanele/randurile delimitatoare gri, deci nu conteaza daca iau punctele de sus/jos - in cazul asta am luat mijlocul (sau coltul stanga jos?) conturului.
@@ -117,7 +131,6 @@ _, binary = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
 
 
 contours, hierarchy = cv2.findContours(binary, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
-img2 = resized.copy()
 
 for ind, cnt in enumerate(contours):
     
@@ -131,14 +144,15 @@ for ind, cnt in enumerate(contours):
 
     print(cv2.contourArea(cnt))
     x, y, w, h = cv2.boundingRect(cnt)
-    cv2.rectangle(img2, (x, y), (x+w, y+h), (255, 0, 0))
-    ROI = img2[y:y+h, x:x+w]
+    tmp = getCoordinatedFromCropped(final_orar, resized, [x, x+w], [y, y+h])
+    cv2.rectangle(final_orar, tmp[0], tmp[1], (255, 0, 0))
+    print(tmp[0][1], tmp[1][1], tmp[0][0], tmp[0][1])
+    ROI = final_orar[tmp[0][1]:tmp[1][1], tmp[0][0]:tmp[1][0]]
     # cv2.imshow('ROI', ROI)
 
     if not os.path.exists(folder):
         os.makedirs(folder)
 
-    ROI = imutils.resize(ROI, width=900)
     cv2.imwrite(os.path.join(folder, name), ROI)
 
     # if cv2.waitKey() == ord(q):
@@ -148,5 +162,5 @@ for ind, cnt in enumerate(contours):
 # gaseste upper top row points si sa le scada valoarea (aka sa le ridice un pic mai sus). functia findTopLeftPointOrar
 # o sa fie utila pt asta
 
-cv2.imshow('img2', img2)
+cv2.imshow('final_orar', final_orar)
 cv2.waitKey()
