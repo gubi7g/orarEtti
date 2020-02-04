77f0fe34e5052d7f71e866caf856f926c4004ae6
Gabriel Galer <gabriel.galer@primesourcing.ro>
2020-02-04 15:07:58 +0200

    to verify if 08-09 interval exists

--- parseCsv.py
+++ parseCsv.py
@@ -1,83 +1,155 @@
 import json
 import csv
 import re
-
+import xlsxwriter
+from os.path import basename, splitext, join
+from os import listdir
 
 def findIndexGrupa(orar, listaGrupe):
     return orar[1].index(listaGrupe[0]), orar[1].index(listaGrupe[-1])
 
+def buildOrar(path, writeToFile=True):
+    orar = []
+    with open(path) as f:
+        reader = csv.reader(f)
 
-orar = []
-with open('res/orar_an3.csv') as f:
-    reader = csv.reader(f)
+        for row in reader:
+            orar.append(row)
 
-    for row in reader:
-        orar.append(row)
 
-# taie chestiile useless
-orar = orar[:70]
+    # serii & re patterns
+    serii = list(filter(None, orar[0]))
+    pattern_serie = re.compile(r'Anul \w{1,3} Seria \w{1}')
 
-# serii & re patterns
-serii = list(filter(None, orar[0]))
-pattern_serie = re.compile(r'Anul \w{1,3} Seria \w{1}')
+    # lista cu toate seriile si numarul de serii
+    serii = [x for x in serii if pattern_serie.findall(x)]
+    nr_serii = len(serii)
 
-# lista cu toate seriile si numarul de serii
-serii = [x for x in serii if pattern_serie.findall(x)]
-# print(serii)
-nr_serii = len(serii)
+    # grupe & re patterns
+    grupe = list(filter(None, orar[1]))
+    pattern_grupa = re.compile(r'4\d{2}\w{1,2}')
+    grupe = [x for x in grupe if pattern_grupa.findall(x)]
+    nr_grupe = len(grupe)
 
-# grupe & re patterns
-grupe = list(filter(None, orar[1]))
-pattern_grupa = re.compile(r'4\d{2}\w{1,2}')
-grupe = [x for x in grupe if pattern_grupa.findall(x)]
-# print(grupe)
-nr_grupe = len(grupe)
+    intervaleOre = [f'{i:02}-{(i+1):02}' for i in range(8, 21)]
+    dict_serii = [{'serie': x, 'numarGrupe': 0, 'grupe': []} for x in 'ABCDEFG']
 
-ore_posibile = [f'{i:02}-{(i+1):02}' for i in range(8, 21)]
-print(ore_posibile)
-dict_serii = [{'serie': x, 'numarGrupe': 0, 'grupe': []} for x in 'ABCDEFG']
-# print(dict_serii)
+    for d in dict_serii:
+        letter = d['serie']
+        tmp_sum = 0
+        for grupa in grupe:
+            if(letter == grupa[3]):
+                tmp_sum = tmp_sum + 1
+                d['grupe'].append(grupa)
+        d['numarGrupe'] = tmp_sum
+        d['startIndexSerie'], d['stopIndexSerie'] = findIndexGrupa(orar, d['grupe'])
 
-for d in dict_serii:
-    letter = d['serie']
-    tmp_sum = 0
-    for grupa in grupe:
-        if(letter == grupa[3]):
-            tmp_sum = tmp_sum + 1
-            d['grupe'].append(grupa)
-    d['numarGrupe'] = tmp_sum
-    d['startIndexSerie'], d['stopIndexSerie'] = findIndexGrupa(orar, d['grupe'])
+    indexiIntervaleOrare = [[] for x in range(5)]
+    currStartIndIntOrar = 2
 
-indexiIntervaleOrare = [[] for x in range(5)]
-currentStartInd = 2
+    for ind, zi in enumerate(indexiIntervaleOrare):
+        
+        indexiIntervaleOrare[ind] = [currStartIndIntOrar, currStartIndIntOrar + len(intervaleOre) - 1]
+        currStartIndIntOrar += len(intervaleOre) + 1
+        
 
-for ind, zi in enumerate(indexiIntervaleOrare):
+    orar_final = []
+    for serie in dict_serii:
+        for grupa, indexGrupa in zip(serie['grupe'], range(serie['startIndexSerie'], serie['stopIndexSerie'] + 1)):
+            
+            currentGroup = {}
+            currentGroup['grupa'] = grupa
+            currentGroup['orar'] = {}
+            
+            for indexiInterval, zi in zip(indexiIntervaleOrare, ['luni', 'marti', 'miercuri', 'joi', 'vineri']):
+                currentGroup['orar'][zi] = {}
+                
+                for ora, indexOra in zip(intervaleOre, range(indexiInterval[0], indexiInterval[1]+1)):
+                    
+                    
+                    if orar[indexOra][indexGrupa] != '':
+                        currentGroup['orar'][zi][ora] = {'course': orar[indexOra][indexGrupa]}
+                    
+        
+            orar_final.append(currentGroup)
     
-    indexiIntervaleOrare[ind] = [currentStartInd, currentStartInd + len(ore_posibile) - 1]
-    currentStartInd += len(ore_posibile) + 1
+    if writeToFile:
+        with open(splitext(basename(path))[0] + '.json', 'w') as f:
+            f.write(json.dumps(orar_final))
+    else:
+        return orar_final
     
-print(indexiIntervaleOrare)
-
-orar_final = [{} for x in grupe]
-for grupa, orar_grupa in zip(grupe, orar_final):
-    orar_grupa['grupa'] = grupa
-    orar_grupa['orar'] = {}
-    indexTmpZi = 2
     
-    for zi in ['luni', 'marti', 'miercuri', 'joi', 'vineri']:
-        orar_grupa['orar'][zi] = {}
-
-print(orar_final)
-for serie in dict_serii:
-    for grupa, currentGroup, indexSerie in zip(serie['grupe'], orar_final, range(serie['startIndexSerie'], serie['stopIndexSerie']+1)):
-        for indexiInterval, zi in zip(indexiIntervaleOrare, ['luni', 'marti', 'miercuri', 'joi', 'vineri']):
-            for ora, indexOra in zip(ore_posibile, range(indexiInterval[0], indexiInterval[1]+1)):
-                # print(ora, indexOra)
-                    currentGroup['orar'][zi]['course'] = orar[indexSerie][indexOra]
-                    # print(orar[indexSerie][indexOra])
     
-        orar_final.append(currentGroup)    
+    
+# orar = []
+# with open('res/orar_an3.csv') as f:
+#     reader = csv.reader(f)
 
-# write everything to a file
-with open('orar_an3.json', 'w') as f:
-    f.write(json.dumps(orar_final))
+#     for row in reader:
+#         orar.append(row)
+
+
+# # serii & re patterns
+# serii = list(filter(None, orar[0]))
+# pattern_serie = re.compile(r'Anul \w{1,3} Seria \w{1}')
+
+# # lista cu toate seriile si numarul de serii
+# serii = [x for x in serii if pattern_serie.findall(x)]
+# nr_serii = len(serii)
+
+# # grupe & re patterns
+# grupe = list(filter(None, orar[1]))
+# pattern_grupa = re.compile(r'4\d{2}\w{1,2}')
+# grupe = [x for x in grupe if pattern_grupa.findall(x)]
+# nr_grupe = len(grupe)
+
+# intervaleOre = [f'{i:02}-{(i+1):02}' for i in range(8, 21)]
+# dict_serii = [{'serie': x, 'numarGrupe': 0, 'grupe': []} for x in 'ABCDEFG']
+
+# for d in dict_serii:
+#     letter = d['serie']
+#     tmp_sum = 0
+#     for grupa in grupe:
+#         if(letter == grupa[3]):
+#             tmp_sum = tmp_sum + 1
+#             d['grupe'].append(grupa)
+#     d['numarGrupe'] = tmp_sum
+#     d['startIndexSerie'], d['stopIndexSerie'] = findIndexGrupa(orar, d['grupe'])
+
+# indexiIntervaleOrare = [[] for x in range(5)]
+# currStartIndIntOrar = 2
+
+# for ind, zi in enumerate(indexiIntervaleOrare):
+    
+#     indexiIntervaleOrare[ind] = [currStartIndIntOrar, currStartIndIntOrar + len(intervaleOre) - 1]
+#     currStartIndIntOrar += len(intervaleOre) + 1
+    
+
+# orar_final = []
+# for serie in dict_serii:
+#     for grupa, indexGrupa in zip(serie['grupe'], range(serie['startIndexSerie'], serie['stopIndexSerie'] + 1)):
+        
+#         currentGroup = {}
+#         currentGroup['grupa'] = grupa
+#         currentGroup['orar'] = {}
+        
+#         for indexiInterval, zi in zip(indexiIntervaleOrare, ['luni', 'marti', 'miercuri', 'joi', 'vineri']):
+#             currentGroup['orar'][zi] = {}
+            
+#             for ora, indexOra in zip(intervaleOre, range(indexiInterval[0], indexiInterval[1]+1)):
+                
+                
+#                 if orar[indexOra][indexGrupa] != '':
+#                     currentGroup['orar'][zi][ora] = {'course': orar[indexOra][indexGrupa]}
+                
+    
+#         orar_final.append(currentGroup)    
+
+# # write everything to a file
+# with open('orar_an3.json', 'w') as f:
+#     f.write(json.dumps(orar_final))
+
+for file in listdir('res'):
+    if file.endswith('.csv'):
+        buildOrar(join('res', file))
