8c6a27825926c4ec39153c4deb36db7e78336250
GalerDekstop <gabigaler50@gmail.com>
2020-04-04 13:25:42 +0300

    separation of concerns with views/ + added router

--- client-orar-etti/src/main.js
+++ client-orar-etti/src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
+import OrarFull from './views/OrarFull.vue'
+import Login from './views/Login.vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 import { BootstrapVue } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -10,7 +13,27 @@ Vue.prototype.$http = axios
 
 // Install BootstrapVue
 Vue.use(BootstrapVue)
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes: [
+    {
+      path: '/orar',
+      name: 'orar',
+      component: OrarFull
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    }
+  ],
+  mode: 'history'
+})
+
+
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
